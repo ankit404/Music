0b981363ac629c2c5f3d47b81fe1444bf85fd358
Ankit <ankitraiiv@gmail.com>
2020-09-02 21:36:46 +0530

    n

---
+++ script.js
@@ -0,0 +1,5 @@
+function sequencer() {
+    const kick = new Tone.Player('./sounds/kick-electro01.wav').toMaster();
+    const snare = new Tone.Player('./sounds/snare-lofi02.wav').toMaster();
+    let index = 0;
+}
