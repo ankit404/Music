48126e67ff64bbe94ac522758fd0e4e941804084
Ankit <ankitraiiv@gmail.com>
2020-09-03 21:05:36 +0530

    k

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function sequencer() {
-    const kick = new Tone.Player('./sounds/kick-electro01.wav').toDestination();
-    const snare = new Tone.Player('./sounds/snare-lofi02.wav').toDestination();
-    let index = 0;
-
-    Tone.Transport.scheduleRepeat(repeat, "8n");
-    Tone.Transport.start();
-
-    function repeat() {
-        let step = index % 8;
-        let kickInputs = document.querySelector(`.kick input:nth-child(${step+1})`);
-        let snareInputs = document.querySelector(`.snare input:nth-child(${step+1})`);
-        if (kickInputs.checked) {
-            kick.start();
-        }
-        if (snareInputs.checked) {
-            snare.start();
-        }
-        index++;
-    }
-
-}
-
-sequencer();
+window.addEventListener("load", () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+});
